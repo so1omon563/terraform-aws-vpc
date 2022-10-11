e34246ad196bd3280682b3cf3df1a157d01d2f36
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-10-11 12:57:43 -0700

    Updated security checks

--- modules/flow-logs/iam-flow-logs-policy-doc.tf
+++ modules/flow-logs/iam-flow-logs-policy-doc.tf
@@ -10,6 +10,7 @@ data "aws_iam_policy_document" "ar" {
 }
 
 data "aws_iam_policy_document" "logs" {
+  #checkov:skip=CKV_AWS_111:"Ensure IAM policies does not allow write access without constraints" - This wildcard is intentional.
   statement {
     actions   = ["logs:CreateLogGroup"]
     resources = ["*"]
@@ -22,6 +23,7 @@ data "aws_iam_policy_document" "logs" {
       "logs:DescribeLogGroups",
       "logs:DescribeLogStreams"
     ]
+    #tfsec:ignore:aws-iam-no-policy-wildcards - This wildcard is intentional.
     resources = [
       aws_cloudwatch_log_group.flow-logs.arn,
       format("%s:log-stream:*", aws_cloudwatch_log_group.flow-logs.arn)
--- modules/subnets/nacl-rules.tf
+++ modules/subnets/nacl-rules.tf
@@ -1,18 +1,30 @@
 resource "aws_network_acl_rule" "ipv4_ingress" {
+  #checkov:skip=CKV_AWS_229:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 21" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_230:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 20" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_231:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 3389" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_232:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 22" - Since this is a re-usable module, this needs to be able to be overridden.
+
   count          = length(aws_subnet.subnet) < 1 || var.restrict_nacls ? 0 : 1
   network_acl_id = aws_network_acl.nacl[count.index].id
   egress         = false
   protocol       = -1
-  cidr_block     = local.all_ipv4
-  rule_action    = "allow"
-  rule_number    = local.max_rule_number
+  #tfsec:ignore:aws-vpc-no-public-ingress-acl - This needs to be able to be configured with open ingress if desired.
+  cidr_block  = local.all_ipv4
+  rule_action = "allow"
+  rule_number = local.max_rule_number
 }
 
 resource "aws_network_acl_rule" "ipv6_ingress" {
-  count           = length(aws_subnet.subnet) < 1 || var.restrict_nacls ? 0 : 1
-  network_acl_id  = aws_network_acl.nacl[count.index].id
-  egress          = false
-  protocol        = -1
+  #checkov:skip=CKV_AWS_229:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 21" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_230:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 20" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_231:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 3389" - Since this is a re-usable module, this needs to be able to be overridden.
+  #checkov:skip=CKV_AWS_232:"Ensure no NACL allow ingress from 0.0.0.0:0 to port 22" - Since this is a re-usable module, this needs to be able to be overridden.
+
+  count          = length(aws_subnet.subnet) < 1 || var.restrict_nacls ? 0 : 1
+  network_acl_id = aws_network_acl.nacl[count.index].id
+  egress         = false
+  protocol       = -1
+  #tfsec:ignore:aws-vpc-no-public-ingress-acl - This needs to be able to be configured with open ingress if desired.
   ipv6_cidr_block = local.all_ipv6
   rule_action     = "allow"
   rule_number     = aws_network_acl_rule.ipv4_ingress[count.index].rule_number - 1
@@ -23,16 +35,18 @@ resource "aws_network_acl_rule" "ipv4_egress" {
   network_acl_id = aws_network_acl.nacl[count.index].id
   egress         = true
   protocol       = -1
-  cidr_block     = local.all_ipv4
-  rule_action    = "allow"
-  rule_number    = local.max_rule_number
+  #tfsec:ignore:aws-vpc-no-public-egress-acl - This needs to be able to be configured with open egress if desired.
+  cidr_block  = local.all_ipv4
+  rule_action = "allow"
+  rule_number = local.max_rule_number
 }
 
 resource "aws_network_acl_rule" "ipv6_egress" {
-  count           = length(aws_subnet.subnet) < 1 || var.restrict_nacls ? 0 : 1
-  network_acl_id  = aws_network_acl.nacl[count.index].id
-  egress          = true
-  protocol        = -1
+  count          = length(aws_subnet.subnet) < 1 || var.restrict_nacls ? 0 : 1
+  network_acl_id = aws_network_acl.nacl[count.index].id
+  egress         = true
+  protocol       = -1
+  #tfsec:ignore:aws-vpc-no-public-egress-acl - This needs to be able to be configured with open egress if desired.
   ipv6_cidr_block = local.all_ipv6
   rule_action     = "allow"
   rule_number     = aws_network_acl_rule.ipv4_ingress[count.index].rule_number - 1
