93be41b29d13d6ac0a5982bf9da7f815d79329b5
Jedidiah Foster <jedidiah.foster@gmail.com>
2024-02-01 11:52:32 -0700

    Updated required Terraform and provider versions

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 * Auto-generated technical documentation is created using [`terraform-docs`](https://terraform-docs.io/)
 */
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.38"
+      version = ">= 4.0"
     }
   }
 }
